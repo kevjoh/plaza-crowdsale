96e69c9d05c81748c7d758fbda3f57def5a529cf
Kevin Johnson <kevin.johnson@plaza.systems>
2018-03-14 19:47:34 +0700

    update oz

--- contracts/FinalizeAgent.sol
+++ contracts/FinalizeAgent.sol
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, version 2.0: https://github.com/TokenMarketNet/ico/blob/master/LICENSE.txt
  */
 
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
 /**
  * Finalize agent defines what happens at the end of succeseful crowdsale.
--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, version 2.0: https://github.com/TokenMarketNet/ico/blob/master/LICENSE.txt
  */
 
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
 // Temporarily have SafeMath here until all contracts have been migrated to SafeMathLib version from OpenZeppelin
 
